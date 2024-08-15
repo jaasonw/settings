a0ace856abd030a60e0df4036e79d699b94fe0c1
jaasonw <jayywong92@gmail.com>
2024-08-14 17:42:16 -0700

    add remove lvim script

---
+++ lvim/remove-lvim.sh
@@ -0,0 +1,9 @@
+sudo rm -rf ~/.local/share/lunarvim
+
+sudo rm -rf ~/.local/share/lvim/
+
+sudo rm -rf ~/.cache/lvim
+
+sudo rm -rf ~/.config/lvim
+
+curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/uninstall.sh | bash
